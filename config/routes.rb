f5c28d1bb60dacdf209195ca6b06bda05e65470e
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-18 03:38:55 +0800

    fixed db + added controller and view for article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'articles#index'
+
+  get 'articles/index' => 'articles#index'
+  get 'articles/new'
+  get 'articles/edit'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
