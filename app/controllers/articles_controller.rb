76d97a6380efe2909c5fe831b7e80037f7e76094
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-20 07:50:08 +0800

    Added all RESTful functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  # I created a function that performs similar actions
+  # before_action means do this before anything else, then the only is for the functions that will use it
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :author, :body))
+    @article = Article.new(article_params)
     
     if @article.save
       flash[:notice] = "Article was successfully created."
@@ -25,21 +28,32 @@ class ArticlesController < ApplicationController
 
   # finding the right article to edit 
   def edit 
-    @article = Article.find(params[:id])
   end 
 
   # will impact the article
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :author, :body))
+    if @article.update(article_params)
       flash[:notice] = "Article was successfully updated."
       redirect_to article_path(@article)
     else
       render 'edit'
     end
-
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
+  
+  private
+  # functions only accessible to this controller
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :author, :body)
+  end
+
 end
