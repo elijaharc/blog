d8d3dbe9e425698978985dcb0a438dab18f184f4
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-21 13:59:41 +0800

    add blog home page & fixed articles listing style

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  root 'articles#index'
+  root 'pages#home'
 
-  # get '/articles' => 'articles#index'
-  # get '/articles/:id' => 'articles#show', as: 'show_article'
-  # get '/article/new' => 'articles#new', as: 'new_article'
-  # get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
-  # post '/articles' => 'articles#create', as: 'create_article'
-  # patch 'articles/:id' => 'articles#update'
-  # put 'articles/:id' => 'articles#update'
-  # get 'articles/destroy'
+  get '/about' => 'pages#about'
+  get '/home' => 'pages#home'
+
   resources :users
   resources :articles
 
