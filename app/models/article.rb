fc9ec0c012a8573da996ccd59e96430b95786f0d
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-22 10:19:45 +0800

    removed title uniqueness

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, uniqueness: true
+    validates :title, presence: true
     validates :author, presence: true
     validates :body, presence: true
     # belongs_to :user
